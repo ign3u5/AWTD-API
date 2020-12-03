4afbf14a1b620a54bd1073d3ea1417e9bde335ac
Chris Farmer <WS311471@weston.ac.uk>
2020-12-03 11:54:19 +0000

    Another test

--- user.php
+++ user.php
@@ -1,5 +1,9 @@
 <?php
-    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Origin: http://localhost:4200");   
+    header("Content-Type: application/json; charset=UTF-8");    
+    header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");    
+    header("Access-Control-Max-Age: 3600");    
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");  
     require_once "orchestrators/userMethodOrchestrator.php";
     require_once "handlers/responseHandler.php";
 
