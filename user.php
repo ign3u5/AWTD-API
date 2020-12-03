18b61a1264767993bf4faf265613416286e2f9e0
Chris Farmer <WS311471@weston.ac.uk>
2020-12-03 11:48:01 +0000

    disabling cors

--- user.php
+++ user.php
@@ -1,7 +1,7 @@
 <?php
+    header("Access-Control-Allow-Origin: *");
     require_once "orchestrators/userMethodOrchestrator.php";
     require_once "handlers/responseHandler.php";
-    header('Access-Control-Allow-Origin: *');
 
     $userMethodOrchestrator = new UserMethodOrchestrator();
     $userMethodOrchestrator->Handle()->SendResponse();
