b59d628b068e4000559b843bcb64919471097635
Chris Farmer <WS311471@weston.ac.uk>
2020-12-03 11:57:23 +0000

    another test

--- user.php
+++ user.php
@@ -3,7 +3,7 @@
     header("Content-Type: application/json; charset=UTF-8");    
     header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");    
     header("Access-Control-Max-Age: 3600");    
-    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");  
+    header("Access-Control-Allow-Headers: Content-Type, content-type, Access-Control-Allow-Headers, Authorization, X-Requested-With");  
     require_once "orchestrators/userMethodOrchestrator.php";
     require_once "handlers/responseHandler.php";
 
