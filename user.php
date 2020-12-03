d1b98f64621b30e4a4f9e93994838d4120dd235a
Chris Farmer <WS311471@weston.ac.uk>
2020-12-03 10:55:28 +0000

    Attempting to allow localhost in CORS

--- user.php
+++ user.php
@@ -1,6 +1,7 @@
 <?php
     require_once "orchestrators/userMethodOrchestrator.php";
     require_once "handlers/responseHandler.php";
+    header('Access-Control-Allow-Origin: http://localhost');
 
     $userMethodOrchestrator = new UserMethodOrchestrator();
     $userMethodOrchestrator->Handle()->SendResponse();
