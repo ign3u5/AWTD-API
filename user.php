32d0b269055fcdb53da1fb35d9d4e4208a34a22e
Chris Farmer <WS311471@weston.ac.uk>
2020-12-04 13:34:02 +0000

    attempting to expose headers

--- user.php
+++ user.php
@@ -3,8 +3,8 @@
     header("Content-Type: application/json; charset=UTF-8");    
     header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");    
     header("Access-Control-Max-Age: 3600");    
-    header("Access-Control-Allow-Headers: Content-Type, content-type, Access-Control-Allow-Headers, Authorization, X-Requested-With, 'Token', 'token'");  
-    header("Access-Control-Expose-Headers: 'Token', 'token'");
+    header("Access-Control-Allow-Headers: Content-Type, content-type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Token, token");  
+    header("Access-Control-Expose-Headers: Token, token");
     require_once "orchestrators/userMethodOrchestrator.php";
     require_once "handlers/responseHandler.php";
 
