d2aaec305408811a8d456a55e4f14c30e0667747
Chris Farmer <WS311471@weston.ac.uk>
2020-12-07 17:30:04 +0000

    Updating cors policy for CMS testing

--- users.php
+++ users.php
@@ -1,4 +1,10 @@
 <?php
+    header("Access-Control-Allow-Origin: http://localhost:4200");   
+    header("Content-Type: application/json; charset=UTF-8");    
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");    
+    header("Access-Control-Max-Age: 3600");    
+    header("Access-Control-Allow-Headers: Content-Type, content-type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Token");  
+    header("Access-Control-Expose-Headers: Token");
     require_once "orchestrators/usersMethodOrchestrator.php";
     require_once "handlers/responseHandler.php";
 
