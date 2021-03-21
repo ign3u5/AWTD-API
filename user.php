846485134a94b7d7dbd18a262f4323d61c91fd89
Chris <chrisfarmer19801@gmail.com>
2021-03-21 12:04:20 +0000

    Fixing CORS issues for live deployment

--- user.php
+++ user.php
@@ -1,5 +1,5 @@
 <?php
-    header("Access-Control-Allow-Origin: http://localhost:4200");   
+    header("Access-Control-Allow-Origin: http://localhost:4200, https://ws311471.remote.ac");   
     header("Content-Type: application/json; charset=UTF-8");    
     header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");    
     header("Access-Control-Max-Age: 3600");    
