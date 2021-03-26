7341c0611df903ae7ef7b779cf4971fc80eb87d1
Chris Farmer <WS311471@weston.ac.uk>
2021-03-26 15:26:53 +0000

    WIP - Adding in functionality to ignore user if the user has been deleted

--- handlers/tokenHandler.php
+++ handlers/tokenHandler.php
@@ -1,12 +1,17 @@
 <?php
 require_once __DIR__."/../models/token.php";
 require_once __DIR__."/../handlers/responseHandler.php";
+require_once __DIR__."/../controllers/usersController.php";
 
 class TokenHandler
 {
     private $jwtHandler;
+    private $userController;
     public function __construct()
     {
+        $database = new Database();
+        $connection = $database->getConnection();
+        $this->userController = new UsersController($connection);
         $this->jwtHandler = new JWTHandler();
     }
     public function GetTokenFromHeader()
@@ -27,6 +32,9 @@ class TokenHandler
         $newToken = $newTokenResponse->payload;
         if ($newToken->exp < time())
             return NewResponse(403, "Authentication has expired");
+
+        if ($this->userController->ReadUser($newToken->username)->IsFail())
+            return NewResponse(401, "User no longer exists");
         
         return NewResponseWithPayload(200, "Token is valid and has not expired", $newToken);
     }
