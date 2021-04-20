2a8f9b485cc9d744f0c4bb4028ff321c4c19d539
Chris Farmer <WS311471@weston.ac.uk>
2021-04-20 12:48:34 +0100

    Adding cors to only allow live env

--- users.php
+++ users.php
@@ -1,5 +1,5 @@
 <?php
-    header("Access-Control-Allow-Origin: *");   
+    header("Access-Control-Allow-Origin: https://ws311471.remote.ac");   
     header("Content-Type: application/json; charset=UTF-8");    
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");    
     header("Access-Control-Max-Age: 3600");    
